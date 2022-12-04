a600e2d0423e49f698ed2be33e2486c47c700751
Ivan Kubota <kubota@form.dev>
2022-12-04 15:25:54 +0400

    fourth day!

--- 03/hard.js
+++ 03/hard.js
@@ -1,4 +1,3 @@
-
 document.body.innerText
 	.trim()
 	.split( '\n' )
@@ -12,23 +11,26 @@ document.body.innerText
 	}, {groups: [], current: []})
 	.groups
 	.map(group => ({
-		rucksuckHash: group[0].split('').reduce((store, letter)=>{store[letter] = true; return store;}, {} ),
+		rucksackHash: group[0].split('').reduce((store, letter)=>{store[letter] = true; return store;}, {} ),
 		group
 	}))
 	.map(obj => ({
-		rucksuckHash: obj.group[1].split('').reduce((store, letter) => {
-			if(letter in obj.rucksuckHash) store[letter] = true;
+		rucksackHash: obj.group[1].split('').reduce((store, letter) => {
+			if(letter in obj.rucksackHash) store[letter] = true;
 			return store;
 		}, {}),
 		group: obj.group
 	}))
 	.map(obj => ({
-		rucksuckHash: obj.group[2].split('').reduce((store, letter) => {
-			if(letter in obj.rucksuckHash) store[letter] = true;
+		rucksackHash: obj.group[2].split('').reduce((store, letter) => {
+			if(letter in obj.rucksackHash) store[letter] = true;
 			return store;
 		}, {}),
 		group: obj.group
 	}))
-	.map(obj => Object.keys(obj.rucksuckHash)[0])
-	.map((letter, _, __, ___, isLower = letter.toLowerCase() === letter) => letter.charCodeAt(0) - (isLower? 'a' : 'A').charCodeAt(0) + (isLower?1: 27) )
+	.map(obj => Object.keys(obj.rucksackHash)[0])
+	.map((letter, _, __, ___,
+		  isLower = letter.toLowerCase() === letter
+	) =>
+		letter.charCodeAt(0) - (isLower? 'a' : 'A').charCodeAt(0) + (isLower?1: 27) )
 	.reduce((a,b)=>a+b)
---
+++ 04/hard.js
@@ -0,0 +1,20 @@
+document.body.innerText
+	.trim()
+	.split( '\n' )
+	.map(line=>
+		line
+			.split(',')
+			.map(numbers =>
+				numbers
+					.split('-')
+					.map(Number)
+
+			).map(arr=>({from:arr[0], to: arr[1]}))
+	)
+	.filter(pair =>
+		!(
+			(pair[0].from > pair[1].to) ||
+			(pair[0].to < pair[1].from)
+		)
+	)
+	.length
